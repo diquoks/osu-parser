2d2735cd48ceedbadc99652cd9cebc74d4ce7380
Denis Titovets <den232titovets@yandex.ru>
2025-10-29 17:39:41 +0300

    Улучшение сборки `.exe`-файла

--- build/build.sh
+++ build/build.sh
@@ -1,5 +1,7 @@
 rm -rf ../src/__pycache__
 rm -rf ../src/logs
 rm -rf ../src/__init__.py
-pyinstaller --debug imports --noconfirm --onefile --windowed --uac-admin --icon "../src/assets/icons/application.ico" --name "osu-parser" --add-data "../src;."  "../src/main.pyw"
+pyinstaller --distpath "osu-parser" --debug imports --noconfirm --onefile --windowed --uac-admin --icon "../src/assets/icons/application.ico" --name "osu-parser" --add-data "../src;."  "../src/main.pyw"
 touch ../src/__init__.py
+rm -rf build
+rm -rf osu-parser.spec
