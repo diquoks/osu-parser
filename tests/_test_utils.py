cafb6464322a5a98738241295ea520a9ebd96a4b
Denis Titovets <den232titovets@yandex.ru>
2026-01-07 13:53:42 +0300

    Улучшение тестирования

--- tests/_test_utils.py
+++ tests/_test_utils.py
@@ -1,7 +1,22 @@
+import pydantic
 import pyquoks
 
 import src.data
+import src.models
 
+
+# region models.py
+
+class TestData(pydantic.BaseModel):
+    beatmap_id: int
+    beatmapset_id: int
+    message: str
+    ruleset: src.models.Ruleset
+    score_id: int
+    user_id: int
+
+
+# endregion
 
 # region data.py
 
--- tests/test_query.py
+++ tests/test_query.py
@@ -30,11 +30,64 @@ class TestQuery(pyquoks.test.TestCase):
             ),
         )
 
-        cls._USER_ID = 31760756
-        cls._RULESET = src.models.Ruleset.OSU
-        cls._BEATMAP_ID = 75
-        cls._BEATMAPSET_ID = 1
-        cls._SCORE_ID = 6016860181
+        cls._TEST_DATA = [
+            tests._test_utils.TestData(
+                beatmap_id=8708,
+                beatmapset_id=184,
+                message="peppy",
+                ruleset=src.models.Ruleset.OSU,
+                score_id=466596,
+                user_id=2,
+            ),
+            tests._test_utils.TestData(
+                beatmap_id=4792235,
+                beatmapset_id=2253116,
+                message="diquoks",
+                ruleset=src.models.Ruleset.MANIA,
+                score_id=5822098788,
+                user_id=31760756,
+            ),
+            tests._test_utils.TestData(
+                beatmap_id=557815,
+                beatmapset_id=241526,
+                message="empty",
+                ruleset=src.models.Ruleset.OSU,
+                score_id=294675820,
+                user_id=7083771,
+            ),
+            tests._test_utils.TestData(
+                beatmap_id=1811527,
+                beatmapset_id=866472,
+                message="osu",
+                ruleset=src.models.Ruleset.OSU,
+                score_id=3427873257,
+                user_id=7562902,
+            ),
+            tests._test_utils.TestData(
+                beatmap_id=5033325,
+                beatmapset_id=2334877,
+                message="taiko",
+                ruleset=src.models.Ruleset.TAIKO,
+                score_id=5614060049,
+                user_id=31148838,
+            ),
+            tests._test_utils.TestData(
+                beatmap_id=3172816,
+                beatmapset_id=1552869,
+                message="catch",
+                ruleset=src.models.Ruleset.CATCH,
+                score_id=3838127929,
+                user_id=8172283,
+            ),
+            tests._test_utils.TestData(
+                beatmap_id=5269878,
+                beatmapset_id=2422085,
+                message="mania",
+                ruleset=src.models.Ruleset.MANIA,
+                score_id=5524092839,
+                user_id=19970192,
+            ),
+        ]
 
     @classmethod
     def tearDownClass(cls) -> None:
@@ -46,15 +99,17 @@ class TestQuery(pyquoks.test.TestCase):
         )
 
     def test_get_raw_beatmap(self) -> None:
-        current_raw_beatmap = self._client.get_raw_beatmap(
-            beatmap_id=self._BEATMAP_ID,
-        )
+        for test_data in self._TEST_DATA:
+            current_raw_beatmap = self._client.get_raw_beatmap(
+                beatmap_id=test_data.beatmap_id,
+            )
 
-        self.assert_type(
-            func_name=self.test_get_raw_beatmap.__name__,
-            test_data=current_raw_beatmap,
-            test_type=src.models.RawBeatmap,
-        )
+            self.assert_type(
+                func_name=self.test_get_raw_beatmap.__name__,
+                test_data=current_raw_beatmap,
+                test_type=src.models.RawBeatmap,
+                message=test_data.message,
+            )
 
     def test_authorize_client(self) -> None:
         self._client.authorize_client()
@@ -63,45 +118,53 @@ class TestQuery(pyquoks.test.TestCase):
             func_name=self.test_authorize_client.__name__,
             test_data=self._config.oauth.access_token,
             test_type=str,
+            message="access_token data in OAuthConfig",
         )
 
         self.assert_type(
             func_name=self.test_authorize_client.__name__,
             test_data=self._config.oauth.expires_timestamp,
             test_type=int,
+            message="expires_timestamp data in OAuthConfig",
         )
 
     def test_get_user(self) -> None:
-        current_user = self._client.get_user(
-            user_id=self._USER_ID,
-            ruleset=self._RULESET,
-        )
+        for test_data in self._TEST_DATA:
+            current_user = self._client.get_user(
+                user_id=test_data.user_id,
+                ruleset=test_data.ruleset,
+            )
 
-        self.assert_type(
-            func_name=self.test_get_user.__name__,
-            test_data=current_user,
-            test_type=src.models.UserExtended,
-        )
+            self.assert_type(
+                func_name=self.test_get_user.__name__,
+                test_data=current_user,
+                test_type=src.models.UserExtended,
+                message=test_data.message,
+            )
 
     def test_get_score(self) -> None:
-        current_score = self._client.get_score(
-            score_id=self._SCORE_ID,
-        )
+        for test_data in self._TEST_DATA:
+            current_score = self._client.get_score(
+                score_id=test_data.score_id,
+            )
 
-        self.assert_type(
-            func_name=self.test_get_user.__name__,
-            test_data=current_score,
-            test_type=src.models.Score,
-        )
+            self.assert_type(
+                func_name=self.test_get_score.__name__,
+                test_data=current_score,
+                test_type=src.models.Score,
+                message=test_data.message,
+            )
 
     def test_get_latest_user_score(self) -> None:
-        current_score = self._client.get_latest_user_score(
-            user_id=self._USER_ID,
-            ruleset=self._RULESET,
-        )
+        for test_data in self._TEST_DATA:
+            current_score = self._client.get_latest_user_score(
+                user_id=test_data.user_id,
+                ruleset=test_data.ruleset,
+            )
 
-        self.assert_type(
-            func_name=self.test_get_latest_user_score.__name__,
-            test_data=current_score,
-            test_type=src.models.Score | None,
-        )
+            self.assert_type(
+                func_name=self.test_get_latest_user_score.__name__,
+                test_data=current_score,
+                test_type=src.models.Score | None,
+                message=test_data.message,
+            )
